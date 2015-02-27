09085797e5ed6bfb668f2e1120d14516a73d5b6d
ramako <wide12@gmail.com>
2015-02-27 13:34:07 +0100

    Arreglando menú
    Permite elegir opciones del menú, añadido un tween a la flecha.

--- gameTitle.js
+++ gameTitle.js
@@ -2,7 +2,6 @@ var PlataformasScroller = PlataformasScroller || {};
 
 PlataformasScroller.GameTitle = function(){};
 var tween;
-var tween2;
 var cursor;
 PlataformasScroller.GameTitle.prototype = {
     preload: function () {
@@ -13,17 +12,28 @@ PlataformasScroller.GameTitle.prototype = {
     this.add.image(0,0,'titleScreen');
     var botonPlay=this.game.add.button(260,320,"play",this.playTheGame,this);
     var botonCreditos = this.game.add.button(260,420,"creditos", this.creditos,this);
-    cursor = this.add.image(230,340,"cursor");
+
+    
         
+    cursor = this.add.image(230,340,"cursor");
     cursor.canMove=true;
-   // tween= this.add.tween(cursor);
+    this.add.tween(cursor)
+            .to({
+                x: cursor.x - 10
+            }, 700, Phaser.Easing.Quadratic.Out)
+            .to({
+                x: cursor.x
+            }, 400, Phaser.Easing.Quadratic.In)
+            .loop()
+            .start();
     },
     
    playTheGame: function(){
 		this.game.state.start("Game");
 	},
     creditos: function () {
-        console.log("Creditos!!")
+    var style = { font: "25px Arial", fill: "#aa0344", align: "center" };
+    var text = this.add.text(this.world.centerX, this.world.centerY, "Versión de desarrollo \n Pablo Casado Arenas", style);
         
     },
     permitirMovimiento: function () {
@@ -38,7 +48,7 @@ PlataformasScroller.GameTitle.prototype = {
               this.permitirMovimiento();
               console.log("Me muevo pabajo!!");
              this.add.tween(cursor).to({ y: 450 }, 200).start();
-
+             console.log(cursor.y);
             
         }
         else if (this.input.keyboard.isDown(Phaser.Keyboard.UP) && cursor.canMove){
@@ -46,12 +56,18 @@ PlataformasScroller.GameTitle.prototype = {
             this.permitirMovimiento();
              console.log("Me muevo parriba!!");
              this.add.tween(cursor).to({ y: 340 }, 200).start();
-
+             console.log(cursor.y);
             
         }
         if(this.input.keyboard.isDown(Phaser.Keyboard.ENTER))
-            this.creditos();
+            if( cursor.y==450)
+            this.creditos(); // Crear nueva imagen con boton "salir" para deshacer los creditos.
+            else
+            this.playTheGame();
+
     }
+    
+
     
     
 }
