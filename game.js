8a69e5d317d1730c6d94eff0d08a68da268edb06
ramako <wide12@gmail.com>
2015-04-02 18:51:26 +0200

    Fichero JSON con mapa y camara que sigue al jugador

--- game.js
+++ game.js
@@ -8,12 +8,13 @@ PlataformasScroller.Game.prototype = {
         this.load.image('background', 'assets/forest_evening.png');
         this.load.spritesheet('dude', 'assets/dude.png',32,48);
         this.load.audio('backgroundMusic', 'assets/battletheoutsiders.mp3');
-        this.load.image('ground', 'assets/platform.png');
+        this.load.tilemap('testmap', 'assets/mapa.json', null, Phaser.Tilemap.TILED_JSON);  // this loads the json tilemap created with tiled (cachekey, filename, type of tilemap parser)
+        this.load.image('forest-2', 'assets/forest-2.png');
       },
     create: function () {
         this.stage.disableVisibilityChange = true; // No pausa el juego cuando pierde el focus, musica continua sonando.
          this.physics.startSystem(Phaser.Physics.ARCADE);
-        this.add.image(0,0,'background');
+        this.add.tileSprite(0,0,1280,640,'background');
        music= this.add.audio('backgroundMusic');
        player = this.add.sprite(52,  150, 'dude');
        this.physics.enable(player, Phaser.Physics.ARCADE);
@@ -22,16 +23,20 @@ PlataformasScroller.Game.prototype = {
         player.body.collideWorldBounds = true;
         music.loop=true;
         music.play();
-        platforms = this.add.group();
-        platforms.enableBody = true;
-        var ground = platforms.create(0, this.world.height - 64, 'ground');
-        ground.body.immovable = true;
         player.animations.add('left', [0, 1, 2, 3], 10, true);
         player.animations.add('right', [5, 6, 7, 8], 10, true);
         cursors = this.input.keyboard.createCursorKeys();
         
         player.checkWorldBounds=true;
-                
+        
+       mymap = this.add.tilemap('testmap');
+        mymap.addTilesetImage('forest-2');
+        layermain = mymap.createLayer('Layer1');
+        
+        mymap.setCollisionByExclusion([0],true, 'Layer1');
+        
+        this.camera.follow(player);
+        this.world.setBounds(0, 0, 1280, 1280);
         
     },
     dead: function () {
@@ -42,15 +47,17 @@ PlataformasScroller.Game.prototype = {
         },
     
     update: function () {
-        this.physics.arcade.collide(player, platforms);
+        this.physics.arcade.collide(player,layermain);
         player.body.velocity.x=0;
 
         player.events.onOutOfBounds.add(this.dead,this);
         
-        if(player.body.touching.down) {
+        if(player.body.onFloor()) {
             player.jumps=2;
             player.jumping=false;
-        }
+          }
+        
+
         
         if(this.input.keyboard.upDuration(Phaser.Keyboard.UP) && player.jumping) {
             player.jumps--;
